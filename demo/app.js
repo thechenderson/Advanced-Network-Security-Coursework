77781cfd13467ac2a270547092bfbefa407be5ca
Adam Malek <malekadas@gmail.com>
2022-03-02 16:11:46 +0100

    Add demo module for the presentation

---
+++ demo/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/reveal.js', express.static(path.join(__dirname, 'node_modules/reveal.js/')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(8080, () => {
+    console.log('Presentation running on port 8080')
+});
+
+module.exports = app;
