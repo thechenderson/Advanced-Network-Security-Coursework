4a88bdb741de1bc407ee18781067b2b62c3686a0
Adam Malek <malekadas@gmail.com>
2022-03-08 17:29:00 +0100

    Log query on GET request to /search

--- app/src/main/kotlin/com/github/thechenderson/advancednetworksecuritycoursework/plugins/Routing.kt
+++ app/src/main/kotlin/com/github/thechenderson/advancednetworksecuritycoursework/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.github.thechenderson.advancednetworksecuritycoursework.plugins
 
+import com.github.thechenderson.advancednetworksecuritycoursework.Logger
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
@@ -7,6 +8,12 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
   // Starting point for a Ktor app:
   routing {
+    get("/search/{query}") {
+      val query = call.parameters["query"] ?: "(empty request)"
+      println(query)
+      Logger.log(query)
+    }
+
     static {
       resource("/", "static/index.html")
       resource("*", "static/index.html")
