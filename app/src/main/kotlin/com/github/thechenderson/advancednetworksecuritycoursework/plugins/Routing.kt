d705ff3c7436274d49b9a00603a27a99841831bb
Adam Malek <malekadas@gmail.com>
2022-03-08 18:05:54 +0100

    Create copy of products.json and allow it to be restored by GET request to /restore

--- app/src/main/kotlin/com/github/thechenderson/advancednetworksecuritycoursework/plugins/Routing.kt
+++ app/src/main/kotlin/com/github/thechenderson/advancednetworksecuritycoursework/plugins/Routing.kt
@@ -4,14 +4,22 @@ import com.github.thechenderson.advancednetworksecuritycoursework.Logger
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import kotlin.io.path.Path
+import kotlin.io.path.toPath
 
 fun Application.configureRouting() {
   // Starting point for a Ktor app:
   routing {
     get("/search/{query}") {
       val query = call.parameters["query"] ?: "(empty request)"
-      println(query)
       Logger.log(query)
+    }
+
+    get("/restore") {
+      restoreProductsCopy()
+      println("File replaced")
     }
 
     static {
@@ -20,4 +28,10 @@ fun Application.configureRouting() {
       resources("static")
     }
   }
+}
+
+private fun restoreProductsCopy() {
+  val copyPath = ClassLoader.getSystemResource("static/files/products_copy.json").toURI().toPath()
+  val originalPath = Path(copyPath.toString().replace("products_copy.json", "products.json"))
+  Files.copy(copyPath, originalPath, StandardCopyOption.REPLACE_EXISTING)
 }
